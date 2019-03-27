be852eb3abff561a38b04689b1dc97c41748d039
Shaleen Jain <shaleen@jain.sh>
2019-03-27 10:56:55 +0530

    add and use ratelimiter crate

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-extern crate grpc;
+#[macro_use]
+extern crate nonzero_ext;
 
 mod protos;
 
 use std::io::Read;
 use std::{io, thread};
+use std::time::Duration;
 
 use futures::sync::oneshot;
 use futures::Future;
 
 use protos::ratelimit::{RateLimitRequest, RateLimitResponse};
 use protos::ratelimit_grpc::{RateLimitService, RateLimitServiceServer};
-use grpc::RequestOptions;
-use grpc::SingleResponse;
+use grpc::{RequestOptions, SingleResponse};
 
 #[derive(Clone)]
-struct RateLimiter;
+struct RateLimitServiceImpl {
+    handle: ratelimit::Handle,
+}
 
-impl RateLimitService for RateLimiter {
+impl RateLimitService for RateLimitServiceImpl {
     fn should_rate_limit(&self, ctx: RequestOptions, req: RateLimitRequest)
         -> SingleResponse<RateLimitResponse> {
         let res = RateLimitResponse::new();
@@ -26,7 +29,16 @@ impl RateLimitService for RateLimiter {
 
 fn main() {
     let port = 50_051;
-    let service = RateLimitServiceServer::new_service_def(RateLimiter);
+    let mut limiter = ratelimit::Builder::new()
+        .capacity(1) //number of tokens the bucket will hold
+        .quantum(1) //add one token per interval
+        .interval(Duration::new(1, 0)) //add quantum tokens every 1 second
+        .build();
+
+    let rate_limiter = RateLimitServiceImpl {
+        handle: limiter.make_handle()
+    };
+    let service = RateLimitServiceServer::new_service_def(rate_limiter);
     let mut server = grpc::ServerBuilder::new_plain();
     server.http.set_port(port);
     server.add_service(service);
