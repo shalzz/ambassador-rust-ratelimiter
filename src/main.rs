8c9e665195ef2f98b191e619ac37acfe070e1cb9
Shaleen Jain <shaleen@jain.sh>
2019-03-29 11:06:57 +0530

    switch to ratelimit_meter

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,95 @@ extern crate nonzero_ext;
 mod protos;
 
 use std::io::Read;
-use std::{io, thread};
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
+use std::{io, thread};
 
 use futures::sync::oneshot;
 use futures::Future;
 
-use protos::ratelimit::{RateLimitRequest, RateLimitResponse};
-use protos::ratelimit_grpc::{RateLimitService, RateLimitServiceServer};
 use grpc::{RequestOptions, SingleResponse};
+use protos::ratelimit::{RateLimitRequest, RateLimitResponse, RateLimitResponse_Code};
+use protos::ratelimit_grpc::RateLimitService;
 
-#[derive(Clone)]
+use ratelimit_meter::{KeyedRateLimiter, LeakyBucket};
+
+#[derive(Clone, Debug)]
 struct RateLimitServiceImpl {
-    handle: ratelimit::Handle,
+    limiter: Box<KeyedRateLimiter<String, LeakyBucket>>,
 }
 
 impl RateLimitService for RateLimitServiceImpl {
-    fn should_rate_limit(&self, ctx: RequestOptions, req: RateLimitRequest)
-        -> SingleResponse<RateLimitResponse> {
-        let res = RateLimitResponse::new();
+    fn should_rate_limit(
+        &self,
+        _ctx: RequestOptions,
+        req: RateLimitRequest,
+    ) -> SingleResponse<RateLimitResponse> {
+        let mut handle = self.limiter.clone();
+        let mut api_key: String = String::new();
+        let mut user_plan: String = String::from("none");
+
+        for descriptor in req.get_descriptors() {
+            for entry in descriptor.entries.iter() {
+                if entry.key == "x-api-key" {
+                    api_key = entry.value.clone();
+                }
+                if entry.key == "x-user-plan" {
+                    user_plan = entry.value.clone();
+                }
+            }
+        }
+
+        let code = if user_plan == "paid" {
+            match handle.check(api_key) {
+                Ok(()) => RateLimitResponse_Code::OK,
+                Err(e) => RateLimitResponse_Code::OVER_LIMIT,
+            }
+        } else {
+            RateLimitResponse_Code::OVER_LIMIT
+        };
+
+        let mut res = RateLimitResponse::new();
+        res.set_overall_code(code);
         SingleResponse::completed(res)
+    }
+}
+
+impl RateLimitServiceImpl {
+    pub fn create_service<H: RateLimitService + 'static + Send + 'static>(
+        handler: H,
+    ) -> ::grpc::rt::ServerServiceDefinition {
+        //let handler_arc = ::std::sync::Arc::new(Mutex::new(handler));
+        let handler_mutex = Mutex::new(handler);
+        ::grpc::rt::ServerServiceDefinition::new(
+            "/pb.lyft.ratelimit.RateLimitService",
+            vec![::grpc::rt::ServerMethod::new(
+                Arc::new(::grpc::rt::MethodDescriptor {
+                    name: "/pb.lyft.ratelimit.RateLimitService/ShouldRateLimit".to_string(),
+                    streaming: ::grpc::rt::GrpcStreaming::Unary,
+                    req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                }),
+                {
+                    ::grpc::rt::MethodHandlerUnary::new(move |o, p| {
+                        handler_mutex.lock().unwrap().should_rate_limit(o, p)
+                    })
+                },
+            )],
+        )
     }
 }
 
 fn main() {
     let port = 50_051;
-    let mut limiter = ratelimit::Builder::new()
-        .capacity(1) //number of tokens the bucket will hold
-        .quantum(1) //add one token per interval
-        .interval(Duration::new(1, 0)) //add quantum tokens every 1 second
-        .build();
-
     let rate_limiter = RateLimitServiceImpl {
-        handle: limiter.make_handle()
+        limiter: Box::new(KeyedRateLimiter::<String, LeakyBucket>::new(
+            nonzero!(1u32),
+            Duration::from_secs(5),
+        )),
     };
-    let service = RateLimitServiceServer::new_service_def(rate_limiter);
+    let service = RateLimitServiceImpl::create_service(rate_limiter);
+    //let service = RateLimitServiceServer::new_service_def(rate_limiter);
     let mut server = grpc::ServerBuilder::new_plain();
     server.http.set_port(port);
     server.add_service(service);
